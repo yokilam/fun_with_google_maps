c02327b1dfb072e78f5fb155c979e0701a8d6fb9
Yoki Lam <yokeyenlam@ac.c4q.nyc>
2018-02-04 12:43:20 -0500

    initial commit

---
+++ app/src/main/java/nyc/c4q/fun_with_google_maps/MainActivity.java
@@ -0,0 +1,21 @@
+package nyc.c4q.fun_with_google_maps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToMapActivity(View view){
+        Intent intent= new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/nyc/c4q/fun_with_google_maps/MapsActivity.java
@@ -0,0 +1,121 @@
+package nyc.c4q.fun_with_google_maps;
+
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1020);
+        } else {
+            mMap.setMyLocationEnabled(true);
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener <Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+
+                            if (location != null) {
+                                double lat = location.getLatitude();
+                                double lng = location.getLongitude();
+                                mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Market in NYC ").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher_round)));
+                            }
+                        }
+                    });
+        }
+
+        Geocoder geocoder= new Geocoder(getApplicationContext());
+        List<Address> address;
+        LatLng p1= null;
+
+
+        try {
+            address= geocoder.getFromLocationName("3105 Astoria Blvd S, Astoria, NY 11102", 5);
+            if (address != null) {
+                Address location = address.get(0);
+                p1= new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(p1).title("Marker in Astoria - Neptune Diner").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher_round)));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Log.d("onMapClick: " , "" +latLng);
+                mMap.clear();
+                double lng= latLng.longitude;
+                double lag= latLng.latitude;
+                mMap.addMarker(new MarkerOptions().position(new LatLng(lag, lng)).title("currentClick"));
+            }
+        });
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        CameraPosition nyc = CameraPosition.builder()
+//                .target(new LatLng(40.7128,-74.0060))
+//                .zoom(16)
+//                .bearing(0)
+//                .tilt(45)
+//                .build();
+        LatLng nyc = new LatLng(40.7128, -74.0060);
+        mMap.addMarker(new MarkerOptions().position(nyc).title("NYC"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(nyc));
+
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.isCompassEnabled();
+        uiSettings.setMyLocationButtonEnabled(true);
+
+
+    }
+}
